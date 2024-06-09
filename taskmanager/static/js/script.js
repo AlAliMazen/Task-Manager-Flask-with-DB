dde1ce7b2d3e065cc4cc605a182f3b34b1a65cdc
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-06-09 03:07:14 +0200

    adding functionality to create tasks and insert them into database

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,9 +2,19 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
-});
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
-  });
+    // modal initialization
+    let delete_modal = document.querySelectorAll('.modal');
+    M.Modal.init(delete_modal);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
+});
